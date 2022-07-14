9f840df8eea48a9258978641389ae481e3d7e194
shaikhmohammedjabir <shaikhmohammedjabir.5226@gmail.com>
2022-07-14 14:43:32 +0530

    output redirection radio button added and ui arrangement fixed

--- api/request.py
+++ api/request.py
@@ -1,7 +1,6 @@
 from requests.structures import CaseInsensitiveDict
 from requests import request
 from json.decoder import JSONDecodeError
-from justext import justext, get_stoplist
 
 class API:
     __OBJECT=None;
@@ -14,17 +13,15 @@ class API:
     def getObject(self):
         return API.__OBJECT
 
-    def callApi(self,*,url:str,method:str,data:str,token:str='',count:int=1)->None:
+    def callApi(self,*,url:str,method:str,data:str,token:str,count:int=1)->None:
         head=None;
         if token:
             head = CaseInsensitiveDict()
             head["Authorization"] = "Bearer {}".format(token)
+            print(head)
         for i in range(count):
             with request(method,url, json=data,headers=head) as response:
                 try:
                     return response.json()
                 except JSONDecodeError as rje:
-                    html=str(rje)
-                    html=html[html.find('<html lang="en">'):html.find('</html>')+8]
-                    for i in justext(html,stoplist=get_stoplist("English")):
-                        print(i.text)
+                    return rje
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from tkinter import Tk, Label, Button, Entry, OptionMenu, Checkbutton, StringVar, BooleanVar, Text, W, DISABLED, NORMAL,END
+from tkinter import Tk, Label, Button, Entry, OptionMenu, Checkbutton, StringVar, BooleanVar, Text, W, N, NW, DISABLED, NORMAL,END
 from constant.variable import METHOD_TYPES
 from constant.button_and_text_name import *
 from api.request import API;
@@ -9,36 +9,44 @@ from validators import url
 class PostMan(Tk):
     def __init__(self):
         super().__init__(className=APPLICATION_TITLE)
+        self.__initialApplication();
+
+        self.mainloop();
+
+    def __initialApplication(self):
         self.geometry(APPLICATION_SIZE);
         self.minsize(*tuple(map(lambda x: int(x), APPLICATION_SIZE.split("x"))));
 
         # API METHOD TYPE
-        self.api_method_option=StringVar();
+        self.api_method_option = StringVar();
         self.api_method_option.set(GET);
-        #TOKEN ENTRY FIELD ENABLE OR DISABLED
-        self.token_radio_value=StringVar();
+        # TOKEN ENTRY FIELD ENABLE OR DISABLED
+        self.token_radio_value = StringVar();
         self.token_radio_value.set(NORMAL);
-        #API RESPONSE
+        # API RESPONSE
         self.api_response_value = StringVar();
 
-        self.__packAndBind(OptionMenu(self,self.api_method_option,*METHOD_TYPES),row=0,column=0);
-        self.__packAndBind(Entry(self,name=URL_ENTRY),row=0,column=1,ipadx=200,ipady=8,pady=10);
-        self.__packAndBindWithEventListener(event=Button(self, text=EXECUTE,name=BUTTON,bg=GREEN),function=self.__callAPI,row=0, column=2);
+        self.__packAndBind(Entry(self, name=URL_ENTRY), row=0, column=0, ipadx=210, ipady=8, pady=10);
+        self.__packAndBind(OptionMenu(self, self.api_method_option, *METHOD_TYPES), row=0, column=1);
+        self.__packAndBindWithEventListener(event=Button(self, text=EXECUTE, name=BUTTON, bg=GREEN),
+                                            function=self.__callAPI, row=0, column=2);
 
-        #FOR TOKEN INPUT FIELD ENABLE OR DISABLE
-        self.__packAndBind(Label(self,text=TOKEN),row=1,column=0);
-        self.__packAndBindWithEventListener(event=Checkbutton(self,variable=self.token_radio_value,onvalue=DISABLED,offvalue=NORMAL),function=self.__radioONOFF,row=1,column=1,sticky=W);
-        self.__packAndBind(Entry(self,name=TOKEN_ENTRY,state=DISABLED),row=2, column=1,ipady=8,ipadx=200,pady=10,sticky=W,columnspan=3);
-        self.__packAndBind(Text(self,name=RESPONSE_TEXT),row=3,column=1);
-
-        self.mainloop()
+        # FOR TOKEN INPUT FIELD ENABLE OR DISABLE
+        self.__packAndBind(Entry(self, name=TOKEN_ENTRY, state=DISABLED), row=1, column=0, ipady=8, ipadx=210);
+        self.__packAndBindWithEventListener(
+            event=Checkbutton(self, text=TOKEN, variable=self.token_radio_value, onvalue=DISABLED, offvalue=NORMAL,
+                              bg=GREEN), function=self.__radioONOFF, row=1, column=1);
+        self.__packAndBind(Text(self, name=RESPONSE_TEXT), row=2, column=0);
+        self.__packAndBind(
+            event=Checkbutton(self, text=APPEND_RESPONSE_DATA, onvalue=DISABLED, offvalue=NORMAL, bg=RED), row=2,
+            column=1,sticky=N, pady=25);
 
     def __packAndBind(self,event,**kwarg)->None:
-        event.grid(kwarg)
+        event.grid(kwarg);
 
     def __packAndBindWithEventListener(self,*,event,function,**kwarg)->None:
-        self.__bindButtonEvent(event,function)
-        event.grid(kwarg)
+        self.__bindButtonEvent(event,function);
+        self.__packAndBind(event,**kwarg);
 
     def __bindButtonEvent(self,button,function_name)->None:
         button.bind('<Button-1>',function_name);
@@ -49,34 +57,34 @@ class PostMan(Tk):
         button.bind('<KP_Enter>', function_name);
 
     def __callAPI(self,event)->None:
-        nameWidget=self.nametowidget
-        url=nameWidget(URL_ENTRY)
-        token=nameWidget(TOKEN_ENTRY)
+        nameWidget=self.nametowidget;
+        url=nameWidget(URL_ENTRY);
+        token=nameWidget(TOKEN_ENTRY);
         method=self.api_method_option.get();
-        data=loads(nameWidget(RESPONSE_TEXT).get(1.0, END)) if method in METHOD_TYPES[1:-1] else None
+        data=loads(nameWidget(RESPONSE_TEXT).get(1.0, END)) if method in METHOD_TYPES[1:-1] else None;
         if self.__validateEntry(url,token,nameWidget(BUTTON),nameWidget(TOKEN_ENTRY)['state']):
-            response=API().callApi(url=url.get(), method=method,token=token.get(),data=data)
-            nameWidget(RESPONSE_TEXT).insert(END,dumps(response,indent=4))
+            response=API().callApi(url=url.get(), method=method,token=token.get(),data=data);
+            nameWidget(RESPONSE_TEXT).insert(END,dumps(response,indent=4));
 
     def __radioONOFF(self,event)->None:
-        self.nametowidget(TOKEN_ENTRY)['state']=self.token_radio_value.get()
+        self.nametowidget(TOKEN_ENTRY)['state']=self.token_radio_value.get();
 
     def __validateEntry(self,request_url,token,button,token_entry)->bool:
-        flag = True
+        flag = True;
         if not url(request_url.get()):
-            request_url['highlightbackground']=RED
+            request_url['highlightbackground']=RED;
             flag = False;
         else:
-            request_url['highlightbackground']=GREEN
+            request_url['highlightbackground']=GREEN;
         if token_entry==NORMAL and not token.get():
-            token['highlightbackground']=RED
+            token['highlightbackground']=RED;
             flag = False;
         elif token_entry==NORMAL:
-            token['highlightbackground']=GREEN
+            token['highlightbackground']=GREEN;
         if not flag:
-            button.configure(bg=YELLOW)
+            button.configure(bg=YELLOW);
             return flag;
-        button.configure(highlightbackground=GREEN)
+        button.configure(highlightbackground=GREEN);
         return flag;
 
 
